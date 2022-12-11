5dfea28a2a217ea84abddeea6e28409d97c8d186
Danny <danny.moore@digital.homeoffice.gov.uk>
2022-12-11 23:06:17 +0000

    solve day 4 part 2

--- python/04/sections.py
+++ python/04/sections.py
@@ -5,16 +5,24 @@ pairs = [line.strip().split(",") for line in lines]
 def assignment_to_range(assignment):
     bounds = [int(x) for x in assignment.split("-")]
     return range(bounds[0], bounds[1] + 1)
+    
+# part 1
+full_overlaps = 0
+# part 2
+overlapping_pairs = 0
 
-count = 0
 
 for pair in pairs:
     ranges = [assignment_to_range(assignment) for assignment in pair]
     intersection = set(ranges[0]).intersection(set(ranges[1]))
 
-    if len(intersection) == min([len(range) for range in ranges]):
-        count += 1
+    if(len(intersection) > 0):
+        overlapping_pairs += 1
 
-print(count)
+    if len(intersection) == min([len(range) for range in ranges]):
+        full_overlaps += 1
+
+print(full_overlaps)
+print(overlapping_pairs)
     
 
