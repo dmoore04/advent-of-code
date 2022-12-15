5119c632a9f8d4a4848f08e106d79a42385fcc74
Danny <danny.moore@digital.homeoffice.gov.uk>
2022-12-15 12:21:15 +0000

    solve day 6, part 2

--- python/06/packets.py
+++ python/06/packets.py
@@ -1,11 +1,14 @@
 input = open("input.txt", "r").read()
 
-def find_packet_marker(input):
-    for i in range(0, len(input)-3):
-        marker = input[i:i+4]
-        if len(set(marker)) == 4:
-            return i+4
+def find_marker(input: str, length: int= 4):
+    for i in range(0, len(input[0:-length])):
+        marker_end = i + length
+        marker = input[i:marker_end]
+        if len(set(marker)) == length:
+            return marker_end
     return None
-
-print(find_packet_marker(input))
+# part 1
+print(find_marker(input))
+# part 2
+print(find_marker(input, 14))
 
